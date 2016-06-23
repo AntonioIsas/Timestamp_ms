e5945b33fb548992390cffd4a4f01b6fb179e79e
Antonio Isas <antonioisas87@gmail.com>
2016-06-23 16:39:38 +0000

    Timestamp MS

---
+++ timestamp_ms.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+
+app.use(express.static('public') );
+
+app.get('/',function(req,res){
+  res.sendFile('index.html');
+});
+
+
+app.get('/:TIME', function(req, res){
+    var input = req.params.TIME;
+    var date;
+    if( isNaN(input) ){
+        date = new Date(req.params.TIME);
+    } else {
+        date = new Date();
+        date.setTime(input);
+    }
+    
+    var result = { 
+        "unix": date.getTime(),
+        "natural": date.toDateString() === 'Invalid Date'?null:date.toDateString()
+    }
+    
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result));
+});
+
+app.listen(8080);
